c6d63ed6827d2f88f4d516ecf6ac74768c1ddeb7
ivo <ivoheeg@gmail.com>
2025-09-02 15:38:46 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,17 +3,31 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from datetime import datetime, timedelta
 from sqlalchemy.sql import func
-import approve
+import approve, time
 
 
 
 app = Flask(__name__)
 CORS(app)
 
+ip_requests = {}
+LIMIT_SECONDS = 14400  # 1 minuut
+
 # Database configuratie
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///posts.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+
+def check_ip_limit():
+    ip = request.remote_addr
+    now = time.time()
+
+    if ip in ip_requests:
+        last_request = ip_requests[ip]
+        if now - last_request < LIMIT_SECONDS:
+            return False  # te snel opnieuw
+    ip_requests[ip] = now
+    return True  # mag doorgaan
 
 # Model
 class Post(db.Model):
@@ -45,6 +59,10 @@ def get_posts():
 # Nieuwe post toevoegen
 @app.route("/api/posts", methods=["POST"])
 def add_post():
+    if not check_ip_limit():
+        return jsonify({"status":"rejected","reason":"Too many requests, wait a bit"}), 429
+    
+
     data = request.json
     if not data or "title" not in data or "content" not in data:
         return jsonify({"status":"error","reason": "Invalid input"}), 400
@@ -59,6 +77,10 @@ def add_post():
         return jsonify({"status":"approved","message": "Post created successfully!", "post": new_post.to_dict()}), 201
     elif approved.lower() == "bad":
         return jsonify({"status":"rejected","reason":"Post rejected by AI moderation"}), 200
+    elif approved.lower() == "english":
+        return jsonify({"status":"rejected","reason":"Post is not in English"})
+    elif approved.lower() == "lang err":
+        return jsonify({"status":"rejected","reason":"unknown language error: language not detected"})
     elif approved.lower() == "too long":
         return jsonify({"status":"rejected","reason":"Post is too long"})
     else:
